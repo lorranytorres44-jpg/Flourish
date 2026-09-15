3d6f502b5b4429db94a2a9d540e05809c86fafc8
nepo <pedroca.borges013@gmail.com>
2026-09-15 20:39:44 -0300

    chore: adicionar script iniciar-localhost.bat

---
+++ iniciar-localhost.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+chcp 65001 > nul
+echo ========================================================
+echo   Iniciando Flowrish no Localhost (Porta 8080)
+echo ========================================================
+echo.
+cd /d "%~dp0"
+start http://localhost:8080
+node server.js
+pause
